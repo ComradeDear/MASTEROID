214b8dd0bd017e3cdef370c48c76acff00d9be4f
Dear Comrade <86433628+ComradeDear@users.noreply.github.com>
2021-08-27 13:00:51 +0530

    Update awake.py

--- userbot/plugins/awake.py
+++ userbot/plugins/awake.py
@@ -15,26 +15,18 @@ from platform import python_version , uname
 
 ALIVE_PIC = Config.ALIVE_PHOTTO
 if ALIVE_PIC is None :
-    ALIVE_PIC = "https://telegra.ph/file/feadbe63818bc695c0417.jpg" # we will change this later
+    ALIVE_PIC = "https://telegra.ph/file/27429a8aa60255b15f77e.jpg"
 
 DEFAULTUSER = str ( ALIVE_NAME ) if ALIVE_NAME else "Set ALIVE_NAME in config vars in Heroku"
 
 ALIVE_MESSAGE = Config.ALIVE_MSG
-ALIVE_MESSAGE = "**🔱Venom Zinda Tha....Zinda Hai....Aur Zinda Rahega🔱 \n\n\n**"
+ALIVE_MESSAGE = "**MASTEROID IS AWAKE... \n\n\n**"
 ALIVE_MESSAGE += "`My Bot Status \n\n\n`"
-ALIVE_MESSAGE += f"`Kernel Type: Monolithic(linux) \n\n`"
-ALIVE_MESSAGE += f"`Kernel: 2.2.9  \n\n`"
-ALIVE_MESSAGE += f"`KDE: 1.1(Red Hat)  \n\n`"
 ALIVE_MESSAGE += "`Default User Interface: KDE Plasma \n\n`"
-ALIVE_MESSAGE += f"`Support Channel` : @Raganork_Official \n\n"
 if ALIVE_MESSAGE is None :
-    ALIVE_MESSAGE = "**🔱Venom Zinda Tha....Zinda Hai....Aur Zinda Rahega🔱 \n\n\n**"
+    ALIVE_MESSAGE = "**MASTEROID IS AWAKE \n\n\n**"
     ALIVE_MESSAGE += "`My Bot Status \n\n\n`"
-    ALIVE_MESSAGE += f"`Kernel Type: Monolithic(linux) \n\n`"
-    ALIVE_MESSAGE += f"`Kernel: 2.2.9  \n\n`"
-    ALIVE_MESSAGE += f"`KDE: 1.1(Red Hat)  \n\n`"
     ALIVE_MESSAGE += "`Default User Interface: KDE Plasma \n\n`"
-    ALIVE_MESSAGE += f"`Support Channel` : @Raganork_Official \n\n"
     ALIVE_MESSAGE += f"`ᎷᎽ ᏰᏫᏕᏕ`: {DEFAULTUSER} \n\n "
 
 
